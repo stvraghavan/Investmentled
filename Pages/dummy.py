c15ec3ae9b6ee17bf8ea8c1aeec9702bee17ba28
Tilak Vijayaraghavan <stvraghavan@gmail.com>
2022-08-25 09:40:07 +0530

    Editing work

--- Pages/dummy.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-col1, col2= st.columns([3,1])
-
-with col1:
-    stocks = st.text_input("Enter the list of stocks you want")
-    tickers = stocks.split(",")
-    print(type(tickers))
--- dummy.py
+++ dummy.py
@@ -1,3 +1,8 @@
-#from Functions import *
-#data = get_data()
-#print(data['SYMBOL'])
+import streamlit as st
+
+col1, col2= st.columns([3,1])
+
+with col1:
+    stocks = st.text_input("Enter the list of stocks you want")
+    tickers = stocks.split(",")
+    print(type(tickers))
